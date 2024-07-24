01a8d68c040f5d92ece9c1b6bdeeb2a57b37cf9e
throther <atakandogrucu1@gmail.com>
2024-07-24 19:51:57 +0300

    üçgen

---
+++ üçgen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Üçgenin boyutunu giriniz: ");
+            int boyut = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= boyut; i++)
+            {
+                for (int j = boyut; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= (2 * i - 1); k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
