35ac28365f22af4c1d4e063a2db20860a7be31e7
throther <atakandogrucu1@gmail.com>
2024-07-25 03:24:46 +0300

    tersten

---
+++ tersten.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("İfade giriniz:");
+            string ifade = Console.ReadLine();
+            string result = "";
+
+            for (int i = ifade.Length - 1; i >= 0; i--)
+            {
+                result += ifade[i];
+            }
+            Console.WriteLine("Sonuç: " + result);
+            Console.ReadKey();
+        }
+        }
+}
+
+
