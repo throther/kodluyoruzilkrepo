493972d39b749d71d7101c106c15c4fcad83fb3d
throther <atakandogrucu1@gmail.com>
2024-07-25 03:20:14 +0300

    daire

---
+++ daire.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+            static void Main()
+            {
+                Console.Write("Dairenin yarıçapını giriniz: ");
+                int yaricap = int.Parse(Console.ReadLine());
+
+                // Daireyi çiz
+                DaireCizici.Ciz(yaricap);
+                Console.ReadLine();
+            }
+
+        class DaireCizici
+        {
+            public static void Ciz(int yaricap)
+            {
+                double kalinlik = 0.4;
+                double disR = yaricap + kalinlik;
+                double icR = yaricap - kalinlik;
+
+                for (int y = yaricap; y >= -yaricap; --y)
+                {
+                    for (int x = -yaricap; x < disR * 2; x++)
+                    {
+                        double deger = x * x + y * y * (1 / kalinlik);
+
+                        if (deger >= icR * icR && deger <= disR * disR)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        }
+    }
+
